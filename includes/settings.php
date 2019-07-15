8d3cc1f701df8c82145f9f89ea01dddf54139f5a
gokul <gokul@22feettribalww.com>
2019-07-16 00:01:18 +0530

    merged ready file

--- includes/settings.php
+++ includes/settings.php
@@ -1,8 +1,12 @@
 <?php
     // local [gokul]
-    $siteurl = 'http://localhost/html/wakefit/';
-    $assetsurl = 'http://localhost/html/wakefit/assets/';
-    $imagesurl = 'http://localhost/html/wakefit/assets/images/';
-    $compurl = 'http://localhost/html/wakefit/components/';
+    // $siteurl = 'http://localhost/w-fit/';
+    // $assetsurl = 'http://localhost/w-fit/assets/';
+    // $imagesurl = 'http://localhost/w-fit/assets/images/';
+    // $compurl = 'http://localhost/w-fit/components/';
     // ::: ip :::
+    $siteurl = 'http://gokul.pixelglimpse.com/demo/wakefit/';
+    $assetsurl = 'http://gokul.pixelglimpse.com/demo/wakefit/assets/';
+    $imagesurl = 'http://gokul.pixelglimpse.com/demo/wakefit/assets/images/';
+    $compurl = 'http://gokul.pixelglimpse.com/demo/wakefit/components/';
 ?>
