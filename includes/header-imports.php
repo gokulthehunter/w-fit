64543eeee06f3eba4e77bfad8581d2262c8f0d36
gokul <gokul@22feettribalww.com>
2019-07-16 00:09:31 +0530

    merged ready file

--- includes/header-imports.php
+++ includes/header-imports.php
@@ -3,5 +3,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- :::: font ::::-->
 <link href="https://fonts.googleapis.com/css?family=EB+Garamond:400,500,600,700,800|Nunito+Sans:200,300,400,600,700&display=swap" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 <!-- :::: scss compiled css :::: -->
 <link rel="stylesheet" href="<?php echo $assetsurl; ?>css/compiled/style.min.css"/>
