d4a6e7c630049fa5d6cf20e399d24633639b339d
gokul <gokul@22feettribalww.com>
2019-07-15 23:19:18 +0530

    base

--- components/shared/header.php
+++ components/shared/header.php
@@ -176,7 +176,19 @@
 </div>
 
 <!-- Cart Section -->
-<?php include('../components/cart.php') ?>
+<!-- <?php //include('../components/cart.php') ?> -->
+<!-- <div class="call-to-action">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h3>Ready to go to bed?</h3>
+                <div class="button-block">
+                    <a class="white-button shop-now-button" href="#">Shop Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> -->
 
 <div class="master-wrap">
     <div class="master-wrap--overlay"></div>
