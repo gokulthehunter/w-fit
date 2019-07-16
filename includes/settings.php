3f704d7930fc7aff0fde9efa6069ec9493d0fad9
gokul <gokul@22feettribalww.com>
2019-07-17 00:58:23 +0530

    style rk

--- includes/settings.php
+++ includes/settings.php
@@ -1,33 +1,23 @@
 <?php
     // local [gokul]
-<<<<<<< HEAD
-<<<<<<< HEAD
-
     global $compurl;
-=======
->>>>>>> dev-rk
-    $siteurl = 'http://localhost/w-fit/';
-    $assetsurl = 'http://localhost/w-fit/assets/';
-    $imagesurl = 'http://localhost/w-fit/assets/images/';
-    $compurl = 'http://localhost/w-fit/components/';
-<<<<<<< HEAD
-=======
-    $siteurl = 'http://localhost/html/W-FIT/';
-    $assetsurl = 'http://localhost/html/W-FIT/assets/';
-    $imagesurl = 'http://localhost/html/W-FIT/assets/images/';
-    $compurl = 'http://localhost/html/W-FIT/components/';
->>>>>>> dev-rj
+    // $siteurl = 'http://localhost/w-fit/';
+    // $assetsurl = 'http://localhost/w-fit/assets/';
+    // $imagesurl = 'http://localhost/w-fit/assets/images/';
+    // $compurl = 'http://localhost/w-fit/components/';
+
+    // $siteurl = 'http://localhost/html/W-FIT/';
+    // $assetsurl = 'http://localhost/html/W-FIT/assets/';
+    // $imagesurl = 'http://localhost/html/W-FIT/assets/images/';
+    // $compurl = 'http://localhost/html/W-FIT/components/';
     // ::: ip :::
-    // $siteurl = 'http://gokul.pixelglimpse.com/demo/wakefit/';
-    // $assetsurl = 'http://gokul.pixelglimpse.com/demo/wakefit/assets/';
-    // $imagesurl = 'http://gokul.pixelglimpse.com/demo/wakefit/assets/images/';
-    // $compurl = 'http://gokul.pixelglimpse.com/demo/wakefit/components/';
+    $siteurl = 'http://gokul.pixelglimpse.com/demo/wakefit/';
+    $assetsurl = 'http://gokul.pixelglimpse.com/demo/wakefit/assets/';
+    $imagesurl = 'http://gokul.pixelglimpse.com/demo/wakefit/assets/images/';
+    $compurl = 'http://gokul.pixelglimpse.com/demo/wakefit/components/';
 
     // $siteurl = 'http://192.168.10.101/w-fit/';
     // $assetsurl = 'http://192.168.10.101/w-fit/assets/';
     // $imagesurl = 'http://192.168.10.101/w-fit/assets/images/';
     // $compurl = 'http://192.168.10.101/w-fit/components/';
 ?>
-=======
-    // ::: ip :::
->>>>>>> dev-rk
