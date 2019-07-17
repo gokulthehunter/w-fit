f9f8f4ad19362b9c8f6a6a122f08cffa6773882b
LIJO JOSEPH <lijo@LIJOs-MacBook-Air.local>
2019-07-17 12:54:45 +0530

    merged files from master

--- includes/settings.php
+++ includes/settings.php
@@ -1,20 +1,21 @@
 <?php
     // local [gokul]
     global $compurl;
-    // $siteurl = 'http://localhost/w-fit/';
-    // $assetsurl = 'http://localhost/w-fit/assets/';
-    // $imagesurl = 'http://localhost/w-fit/assets/images/';
-    // $compurl = 'http://localhost/w-fit/components/';
+    $siteurl = 'http://localhost/w-fit/';
+    $assetsurl = 'http://localhost/w-fit/assets/';
+    $imagesurl = 'http://localhost/w-fit/assets/images/';
+    $compurl = 'http://localhost/w-fit/components/';
 
     // $siteurl = 'http://localhost/html/W-FIT/';
     // $assetsurl = 'http://localhost/html/W-FIT/assets/';
     // $imagesurl = 'http://localhost/html/W-FIT/assets/images/';
     // $compurl = 'http://localhost/html/W-FIT/components/';
     // ::: ip :::
-    $siteurl = 'http://gokul.pixelglimpse.com/demo/wakefit/';
-    $assetsurl = 'http://gokul.pixelglimpse.com/demo/wakefit/assets/';
-    $imagesurl = 'http://gokul.pixelglimpse.com/demo/wakefit/assets/images/';
-    $compurl = 'http://gokul.pixelglimpse.com/demo/wakefit/components/';
+
+    // $siteurl = 'http://gokul.pixelglimpse.com/demo/wakefit/';
+    // $assetsurl = 'http://gokul.pixelglimpse.com/demo/wakefit/assets/';
+    // $imagesurl = 'http://gokul.pixelglimpse.com/demo/wakefit/assets/images/';
+    // $compurl = 'http://gokul.pixelglimpse.com/demo/wakefit/components/';
 
     // $siteurl = 'http://192.168.10.101/w-fit/';
     // $assetsurl = 'http://192.168.10.101/w-fit/assets/';
